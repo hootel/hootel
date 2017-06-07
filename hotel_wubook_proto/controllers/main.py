215d12e00c31bbc45dff64d0ef47540e051f4519
Alexandre Díaz <dev@redneboa.es>
2017-06-07 13:13:05 +0200

    [WIP] WuBook Prototype

--- hotel_wubook_proto/controllers/main.py
+++ hotel_wubook_proto/controllers/main.py
@@ -24,14 +24,14 @@ _logger = logging.getLogger(__name__)
 
 
 class website_wubook(http.Controller):
-    @http.route(['/wubook/push/reservations'], type='html', cors="*",
+    @http.route(['/wubook/push/reservations'], type='http', cors="*",
                 auth="public", methods=['POST'], website=True, csrf=False)
     def wubook_push_reservations(self, **kwargs):
         _logger.info("WUBOOK PUSH RESERVATIONS")
         _logger.info(kwargs)
         return True
 
-    @http.route(['/wubook/push/rooms'], type='html', cors="*",
+    @http.route(['/wubook/push/rooms'], type='http', cors="*",
                 auth="public", methods=['POST'], website=True, csrf=False)
     def wubook_push_rooms(self, **kwargs):
         _logger.info("WUBOOK PUSH ROOMS")
