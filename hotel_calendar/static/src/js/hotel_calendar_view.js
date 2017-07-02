549a08e5942ab506146488decc895cad6ce2bebe
Alexandre Díaz <dev@redneboa.es>
2017-07-02 19:52:45 +0200

    [FIX] Room Tooltips

--- hotel_calendar/static/src/js/hotel_calendar_view.js
+++ hotel_calendar/static/src/js/hotel_calendar_view.js
@@ -426,25 +426,6 @@ var HotelCalendarView = View.extend({
                 $content: QWeb.render('HotelCalendar.ConfirmPriceChange', qdict)
             }).open();
         });
-        
-        // Extra Info (Rooms)
-        $(this._hcalendar.etable).find('.hcal-cell-room-type-group-item.btn-hcal-3d').on("mouseenter", function(){
-        	var $this = $(this);
-        	var room = self._hcalendar.getRoom($this.parent().data("hcalRoomObjId"));
-        	var qdict = {
-    			'price_from': room.getUserData('price_from'),
-                'inside_rooms': room.getUserData('inside_rooms'),
-                'num_inside_rooms': room.getUserData('inside_rooms').length,
-                'name': room.number
-        	};
-        	$this.tooltip({
-                animation: true,
-                html: true,
-                placement: 'right',
-                title: QWeb.render('HotelCalendar.TooltipRoom', qdict)
-            }).tooltip('show');
-        });
-        //
     },
 
     generate_hotel_calendar: function(){
@@ -499,6 +480,26 @@ var HotelCalendarView = View.extend({
                 reservs.push(nreserv);
             }
             self._hcalendar.setReservations(reservs);
+            self.assign_extra_info_();
+        });
+    },
+    
+    assign_extra_info_: function() {
+        $(self._hcalendar.etable).find('.hcal-cell-room-type-group-item.btn-hcal-3d').on("mouseenter", function(){
+        	var $this = $(this);
+        	var room = self._hcalendar.getRoom($this.parent().data("hcalRoomObjId"));
+        	var qdict = {
+    			'price_from': room.getUserData('price_from'),
+                'inside_rooms': room.getUserData('inside_rooms'),
+                'num_inside_rooms': room.getUserData('inside_rooms').length,
+                'name': room.number
+        	};
+        	$this.tooltip({
+                animation: true,
+                html: true,
+                placement: 'right',
+                title: QWeb.render('HotelCalendar.TooltipRoom', qdict)
+            }).tooltip('show');
         });
     },
 
@@ -833,6 +834,8 @@ var HotelCalendarView = View.extend({
 
             self._hcalendar.pricelist = results['pricelist'];
             self._hcalendar.setReservations(reservs);
+
+            self.assign_extra_info_();
         });
         this.update_buttons_counter();
     },
