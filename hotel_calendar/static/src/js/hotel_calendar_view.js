f7db0aec8b49c98db109f2931497df2213d4f7dc
Alexandre Díaz <dev@redneboa.es>
2017-07-02 19:56:49 +0200

    [FIX] Last commit

--- hotel_calendar/static/src/js/hotel_calendar_view.js
+++ hotel_calendar/static/src/js/hotel_calendar_view.js
@@ -485,7 +485,7 @@ var HotelCalendarView = View.extend({
     },
     
     assign_extra_info_: function() {
-        $(self._hcalendar.etable).find('.hcal-cell-room-type-group-item.btn-hcal-3d').on("mouseenter", function(){
+        $(this._hcalendar.etable).find('.hcal-cell-room-type-group-item.btn-hcal-3d').on("mouseenter", function(){
         	var $this = $(this);
         	var room = self._hcalendar.getRoom($this.parent().data("hcalRoomObjId"));
         	var qdict = {
