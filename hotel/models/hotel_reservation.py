65ea61255b1fc02e44bed387574c81329526d046
Dario Lodeiros <dario@aloxa.eu>
2018-05-17 13:27:16 +0200

    [FIX] multi write reservation prices

--- hotel/models/hotel_reservation.py
+++ hotel/models/hotel_reservation.py
@@ -777,20 +777,7 @@ class HotelReservation(models.Model):
 
     @api.multi
     def write(self, vals):
-        pricesChanged = ('checkin' in vals or 'checkout' in vals or 'discount' in vals)
-        if len(self.env) == 1: #FIX, pass this to before of super with update instead of vals??¿
-            if (pricesChanged and 'reservation_lines' not in vals) or \
-                    not self.reservation_lines: #To allow add tree edit bottom room_lines on folio form
-                for record in self:
-                    checkin = vals.get('checkin', record.checkin)
-                    checkout = vals.get('checkout', record.checkout)
-                    days_diff = date_utils.date_diff(checkin,
-                                                     checkout, hours=False)                                                 
-                rlines = self.prepare_reservation_lines(checkin, days_diff)
-                vals.update({
-                    'reservation_lines': rlines['commands'],
-                    'price_unit':  rlines['total_price'],
-                })
+        pricesChanged = ('checkin' in vals or 'checkout' in vals or 'discount' in vals)           
         vals.update({
             'edit_room': False,
         })
@@ -804,6 +791,17 @@ class HotelReservation(models.Model):
                 if record.reservation_type in ('staff', 'out'):
                     record.update({'price_unit': 0})
                 record.folio_id.compute_invoices_amount()
+        if (pricesChanged and 'reservation_lines' not in vals) or \
+                    not self.reservation_lines: #To allow add tree edit bottom room_lines on folio form
+                for record in self:
+                    checkin = vals.get('checkin', record.checkin)
+                    checkout = vals.get('checkout', record.checkout)
+                    days_diff = date_utils.date_diff(checkin,
+                                                     checkout, hours=False)                                                 
+                    rlines = record.prepare_reservation_lines(checkin, days_diff)
+                    record.update({
+                        'reservation_lines': rlines['commands']
+                    })
         return res
 
     @api.multi
