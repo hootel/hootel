381c95a02e93f193f1c1435a8489701b1679d9bd
Jose Luis <osotranquilo@gmail.com>
2018-12-12 19:38:54 +0100

    [ADD] New module RoomMatik

---
+++ hotel_roommatik/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Jose Luis Algara (Alda hotels) <osotranquilo@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hotel RoomMatik',
+    'description': """
+        Integration of Hootel with the RoomMatik kiosk""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Jose Luis Algara (Alda hotels) <osotranquilo@gmail.com>',
+    'website': 'https://www.aldahotels.com',
+    'depends': [
+        'hotel',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
