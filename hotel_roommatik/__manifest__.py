ddb4dc5555a545fad0015cdf39c77c59b87e2e0e
Jose Luis <osotranquilo@gmail.com>
2018-12-12 19:54:36 +0100

    [ADD] roommatik cummary and category in manifest

--- hotel_roommatik/__manifest__.py
+++ hotel_roommatik/__manifest__.py
@@ -5,10 +5,15 @@
     'name': 'Hotel RoomMatik',
     'description': """
         Integration of Hootel with the RoomMatik kiosk""",
+    'summary': """
+        The integration of Hootel with the RoomMatik kiosk.
+        A series of requests/responses that provide the basic
+        information needed by the kiosk.""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Jose Luis Algara (Alda hotels) <osotranquilo@gmail.com>',
     'website': 'https://www.aldahotels.com',
+    'category': 'Generic Modules/Hotel Management',
     'depends': [
         'hotel',
     ],
