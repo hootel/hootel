a343709264a9af1ac205a4cd6f90710d7df52303
Jose Luis <osotranquilo@gmail.com>
2018-12-20 13:57:21 +0100

    [IMP] Get Reservation

--- hotel_roommatik/models/inherited_hotel_folio.py
+++ hotel_roommatik/models/inherited_hotel_folio.py
@@ -2,15 +2,36 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
-import xml.etree.cElementTree as ET
+import json
 
 
-class Inherited_hotel_folio(models.Model):
+class hotel_folio(models.Model):
 
     _inherit = 'hotel.folio'
 
+    @api.model
+    def rm_get_reservation(self, Code=False):
+        folio_res = self.env['hotel.folio'].search([('id', '=', Code)])
+        json_response = dict()
+        if len(folio_res) > 0:
+            folio_lin = folio_res.room_lines
+            json_response = {
+                'Id': folio_res.id,
+                'Arrival': folio_lin[0]['checkin'],
+                'Departure': folio_lin[0]['checkout'],
+                'Deposit': folio_res.amount_total,
+                'Rooms': dict(),
+                }
+            for i, linea in enumerate(folio_lin):
+                json_response['Rooms'][i] = {
+                     'Id': linea.id,
+                     'Adults': linea.adults,
+                     'IsAvailable': 0,  # Need a function (Clean and no Checkin)
+                     'Price': linea.price_total,
+                     'RoomTypeId': linea.room_type_id.id,
+                     'RoomTypeName': linea.room_type_id.name,
+                     'RoomName': linea.room_id.name,
+                     }
+        json_response = json.dumps(json_response)
 
-@api.model
-def RmGetReservation(self, Code=False):
-    xml_response = ET.Element("GetReservationResult")
-    return xml_response
+        return json_response
