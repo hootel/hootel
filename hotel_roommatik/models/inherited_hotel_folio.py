41b2e88aa5f3402e9059a3389c4bd626cf5648ce
Jose Luis <osotranquilo@gmail.com>
2018-12-21 12:04:22 +0100

    [FIX] Tardo requested changes, Thanks

--- hotel_roommatik/models/inherited_hotel_folio.py
+++ hotel_roommatik/models/inherited_hotel_folio.py
@@ -13,25 +13,24 @@ class HotelFolio(models.Model):
     def rm_get_reservation(self, Code=False):
         folio_res = self.env['hotel.folio'].search([('id', '=', Code)])
         json_response = dict()
-        if len(folio_res) > 0:
+        if any(folio_res):
             folio_lin = folio_res.room_lines
             json_response = {
                 'Id': folio_res.id,
                 'Arrival': folio_lin[0]['checkin'],
                 'Departure': folio_lin[0]['checkout'],
                 'Deposit': folio_res.amount_total,
-                'Rooms': dict(),
                 }
-            for i, linea in enumerate(folio_lin):
-                json_response['Rooms'][i] = {
-                     'Id': linea.id,
-                     'Adults': linea.adults,
+            for i, line in enumerate(folio_lin):
+                json_response.setdefault('Rooms', [i]).append({
+                     'Id': line.id,
+                     'Adults': line.adults,
                      'IsAvailable': 0,  # Need a function (Clean and no Checkin)
-                     'Price': linea.price_total,
-                     'RoomTypeId': linea.room_type_id.id,
-                     'RoomTypeName': linea.room_type_id.name,
-                     'RoomName': linea.room_id.name,
-                     }
+                     'Price': line.price_total,
+                     'RoomTypeId': line.room_type_id.id,
+                     'RoomTypeName': line.room_type_id.name,
+                     'RoomName': line.room_id.name,
+                     })
         json_response = json.dumps(json_response)
 
         return json_response
