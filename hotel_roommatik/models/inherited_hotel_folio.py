3d886109c7097c58d154c2a8e76ea4351462c060
Jose Luis <osotranquilo@gmail.com>
2018-12-21 14:29:40 +0100

    [FIX] Clean imports and code

--- hotel_roommatik/models/inherited_hotel_folio.py
+++ hotel_roommatik/models/inherited_hotel_folio.py
@@ -1,8 +1,9 @@
 # Copyright 2018 Jose Luis Algara (Alda hotels) <osotranquilo@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
 import json
+
+from odoo import api, models
 
 
 class HotelFolio(models.Model):
@@ -10,7 +11,7 @@ class HotelFolio(models.Model):
     _inherit = 'hotel.folio'
 
     @api.model
-    def rm_get_reservation(self, Code=False):
+    def rm_get_reservation(self, Code):
         folio_res = self.env['hotel.folio'].search([('id', '=', Code)])
         json_response = dict()
         if any(folio_res):
@@ -20,17 +21,18 @@ class HotelFolio(models.Model):
                 'Arrival': folio_lin[0]['checkin'],
                 'Departure': folio_lin[0]['checkout'],
                 'Deposit': folio_res.amount_total,
-                }
+            }
             for i, line in enumerate(folio_lin):
                 json_response.setdefault('Rooms', [i]).append({
-                     'Id': line.id,
-                     'Adults': line.adults,
-                     'IsAvailable': 0,  # Need a function (Clean and no Checkin)
-                     'Price': line.price_total,
-                     'RoomTypeId': line.room_type_id.id,
-                     'RoomTypeName': line.room_type_id.name,
-                     'RoomName': line.room_id.name,
-                     })
+                    'Id': line.id,
+                    'Adults': line.adults,
+                    # Need a function (Clean and no Checkin)
+                    'IsAvailable': 0,
+                    'Price': line.price_total,
+                    'RoomTypeId': line.room_type_id.id,
+                    'RoomTypeName': line.room_type_id.name,
+                    'RoomName': line.room_id.name,
+                })
         json_response = json.dumps(json_response)
 
         return json_response
