3d749bb2740a600797364f114cfd6a419a9e2fb2
Jose Luis <osotranquilo@gmail.com>
2020-01-17 11:41:47 +0100

    [FIX] Calculate default values in year changes, and others

--- hotel_ine/wizard/inewizard.py
+++ hotel_ine/wizard/inewizard.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2017-19 Alda Hotels <informatica@aldahotels.com>
+#    Copyright (C) 2017-20 Alda Hotels <informatica@aldahotels.com>
 #                          Jose Luis Algara <osotranquilo@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -30,52 +30,47 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def get_years():
-    year_list = []
-    for i in range(2017, get_year()+1):
-        year_list.append((i, str(i)))
-    return year_list
-
-
-def get_year():
-    now = datetime.datetime.now()
-    return int(now.year)
-
-
-def get_month():
-    now = datetime.datetime.now()
-    month = int(now.month)-1
-    if month <= 0:
-        month = 12
-    return month
-
-
 class Wizard(models.TransientModel):
     _name = 'ine.wizard'
 
     txt_filename = fields.Char()
     txt_binary = fields.Binary()
-
     ine_month = fields.Selection([(1, 'January'), (2, 'February'),
                                   (3, 'March'), (4, 'April'),
                                   (5, 'May'), (6, 'June'), (7, 'July'),
                                   (8, 'August'), (9, 'September'),
                                   (10, 'October'), (11, 'November'),
                                   (12, 'December'), ],
-                                 string='Month', default=get_month())
-    ine_year = fields.Selection(get_years(), default=get_year(), string='Year')
+                                 string='Month',
+                                 default=lambda self:
+                                     (datetime.datetime.now().month - 1)
+                                     if (datetime.datetime.now().month > 1)
+                                     else 12)
 
+    ine_year = fields.Selection(selection='_get_years',
+                                default=lambda self:
+                                    (datetime.datetime.now().year - 2018)
+                                    if (datetime.datetime.now().month > 1)
+                                    else (datetime.datetime.now().year - 2019),
+                                string='Year')
     adr_screen = fields.Char()
     rev_screen = fields.Char()
 
     @api.one
     def generate_file(self):
-        _logger.warning("Start Export INE XML file")
-        last_day = calendar.monthrange(self.ine_year, self.ine_month)[1]
-        ine_start_search = datetime.date(self.ine_year, self.ine_month, 1)
+        for_year = int(self.ine_year) + 2018
+        _logger.warning("Start Export INE XML file for %s of %s",
+                        self.ine_month,
+                        for_year)
+        last_day = calendar.monthrange(for_year, self.ine_month)[1]
+        ine_start_search = datetime.date(for_year, self.ine_month, 1)
         ine_end_search = ine_start_search + datetime.timedelta(days=last_day)
+        _logger.warning("For %s to %s", ine_start_search, ine_end_search)
+
         compan = self.env.user.company_id
         active_room = self.env['hotel.room'].search([('in_ine', '=', True)])
+        _logger.info("%s: Active Rooms", len(active_room))
+
         message = ""
         if not compan.property_name:
             message = 'The NAME of the property is not established'
@@ -85,6 +80,8 @@ class Wizard(models.TransientModel):
             message = 'The VAT is not established'
         if not compan.ine_tourism:
             message = 'The tourism number of the property is not established'
+        if len(active_room) < 1:
+            message = 'NO Active Rooms'
         if message != "":
             raise UserError(message)
             return
@@ -92,7 +89,7 @@ class Wizard(models.TransientModel):
         cabezera = ET.SubElement(encuesta, "CABECERA")
         fecha = ET.SubElement(cabezera, "FECHA_REFERENCIA")
         ET.SubElement(fecha, "MES").text = "%02d" % (self.ine_month)
-        ET.SubElement(fecha, "ANYO").text = str(self.ine_year)
+        ET.SubElement(fecha, "ANYO").text = str(for_year)
         ET.SubElement(cabezera, "DIAS_ABIERTO_MES_REFERENCIA").text = (
             str(last_day))
         ET.SubElement(cabezera, "RAZON_SOCIAL").text = compan.name
@@ -125,14 +122,19 @@ class Wizard(models.TransientModel):
             ('reservation_id.state', '!=', "cancelled"),
             ('reservation_id.reservation_type', '=', 'normal'),
             ])
+        _logger.info("%s: All Rooms Nights", len(all_room_nights))
         room_nights = all_room_nights.filtered(
                     lambda n: (self.get_codeine(n.reservation_id)))
+        _logger.info("%s: Filtered Rooms Nights", len(room_nights))
 
+        if len(room_nights) < 1:
+            message = 'NO Rooms Nights in this period'
         # Creating the empty dictionary system
         dic_tabla = []
         for room_night in room_nights:
             ine_code = self.get_codeine(room_night.reservation_id)
-            if not next((item for item in dic_tabla if item["ine"] == ine_code), False):
+            if not next((item for item in dic_tabla if item["ine"] ==
+                         ine_code), False):
                 for x in range(1, last_day+1):
                     dic_tabla.append({'ine': ine_code,
                                       'dia': x,
@@ -146,7 +148,7 @@ class Wizard(models.TransientModel):
         for dia in range(1, last_day+1):
             pernocta_total.append(0)
             for room_night in room_nights.filtered(
-                    lambda x: x.date == str(self.ine_year)+'-'+str(
+                    lambda x: x.date == str(for_year)+'-'+str(
                     self.ine_month).zfill(2)+'-'+str(dia).zfill(2)):
                 ine_code = self.get_codeine(room_night.reservation_id)
                 for idx, val in enumerate(dic_tabla):
@@ -160,7 +162,8 @@ class Wizard(models.TransientModel):
                 last_stay = 0
 
             if last_stay < dic_tabla[idx]['pernocta']:
-                dic_tabla[idx]['entradas'] += dic_tabla[idx]['pernocta'] - last_stay
+                dic_tabla[idx]['entradas'] += (dic_tabla[idx]['pernocta'] -
+                                               last_stay)
             elif last_stay > dic_tabla[idx]['pernocta']:
                 dic_tabla[idx]['salidas'] += last_stay - dic_tabla[idx][
                                                                     'pernocta']
@@ -207,12 +210,12 @@ class Wizard(models.TransientModel):
 
             habitaci += len(active_room)
             habitaci -= self.env['hotel.reservation.line'].search([
-                ('date', '=', str(self.ine_year)+'-'+str(
+                ('date', '=', str(for_year)+'-'+str(
                     self.ine_month).zfill(2)+'-'+str(dia).zfill(2)),
                 ('reservation_id.reservation_type', '!=', 'normal'),
                 ], count=True)
             for room_night in room_nights.filtered(lambda x: x.date == str(
-                                            self.ine_year)+'-'+str(
+                                            for_year)+'-'+str(
                                             self.ine_month).zfill(2)+'-'+str(
                                             dia).zfill(2)):
                 ingresos += room_night.price
@@ -256,7 +259,11 @@ class Wizard(models.TransientModel):
                                                                         dindi)
             ET.SubElement(habitaciones_m,
                           "HABITACIONES_OTRAS").text = str(otras)
-
+        if hab_vend < 1:
+            message = 'NO Rooms Nights'
+        if message != "":
+            raise UserError(message)
+            return
         precios = ET.SubElement(encuesta, "PRECIOS")
         ET.SubElement(precios,
                       "REVPAR_MENSUAL").text = str(round(ingresos/habitaci, 2))
@@ -311,7 +318,7 @@ class Wizard(models.TransientModel):
         xmlstr += ET.tostring(encuesta).decode('utf-8')
         return self.write({
              'txt_filename': 'INE_'+str(self.ine_month)+'_'+str(
-                                                self.ine_year) + '.' + 'xml',
+                                                for_year) + '.' + 'xml',
              'adr_screen': 'ADR en el mes de la encuesta: '+str(
                  round(ingresos/habitaci, 2)) + '€ y ',
              'rev_screen': ' RevPar : '+str(round(ingresos/hab_vend, 2))+'€',
@@ -329,3 +336,11 @@ class Wizard(models.TransientModel):
                 if l.code_ine_id:
                     response = l.code_ine_id.code
         return response
+
+    @api.model
+    def _get_years(self):
+        year_list = []
+        total_years = datetime.datetime.now().year - 2018
+        for i in range(total_years + 1):
+            year_list.append((i, str(i+2018)))
+        return year_list
