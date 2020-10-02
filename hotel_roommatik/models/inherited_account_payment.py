7123e52e8f15c5f1d09959b92328fa890545da90
Jose Luis <osotranquilo@gmail.com>
2020-10-02 10:57:47 +0200

    [ADD] Journals in Roommatik payments

--- hotel_roommatik/models/inherited_account_payment.py
+++ hotel_roommatik/models/inherited_account_payment.py
@@ -1,22 +1,34 @@
 # Copyright 2017  Dario Lodeiros
+# Copyright 2020  Jose Luis Algara
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.exceptions import except_orm
-from odoo import models, fields, api, _
+# from odoo.exceptions import except_orm
+from odoo import models, api
+import logging
+
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     @api.model
     def rm_add_payment(self, code, payment):
+        _logger = logging.getLogger(__name__)
+        res_company_obj = self.env.user.company_id
         reservation = self.env['hotel.reservation'].search([
             '|', ('localizator', '=', code),
             ('folio_id.name', '=', code)])
         if not reservation:
             return False
+        if (res_company_obj.rm_cash_journal.id == 0 or
+                res_company_obj.rm_cash_journal.id == 0):
+            _logger.critical("RoomMatik CONFIG ERROR need Journal IDs")
+            return False
         if reservation:
             for cashpay in payment['CashPayments']:
+                _logger.info("RoomMatik Payment Cash in Folio: [%s] of %s",
+                             reservation.folio_id,
+                             cashpay['Amount'].replace(',', '.'))
                 vals = {
-                    'journal_id': 7,  # TODO:config setting
+                    'journal_id': res_company_obj.rm_cash_journal.id,  # 7
                     'partner_id': reservation.partner_invoice_id.id,
                     'amount': float(cashpay['Amount'].replace(',', '.')),
                     'payment_date': cashpay['DateTime'],
@@ -29,8 +41,11 @@ class AccountPayment(models.Model):
                 }
                 pay = self.create(vals)
             for cashpay in payment['CreditCardPayments']:
+                _logger.info("RoomMatik Payment C.Card in Folio: [%s] of %s",
+                             reservation.folio_id,
+                             cashpay['Amount'].replace(',', '.'))
                 vals = {
-                    'journal_id': 15,  # TODO:config setting
+                    'journal_id': res_company_obj.rm_credit_journal.id,  # 15
                     'partner_id': reservation.partner_invoice_id.id,
                     'amount': float(cashpay['Amount'].replace(',', '.')),
                     'payment_date': cashpay['DateTime'],
--- hotel_roommatik/models/inherited_res_company.py
+++ hotel_roommatik/models/inherited_res_company.py
@@ -1,5 +1,6 @@
 # Copyright 2017  Alexandre Díaz
 # Copyright 2017  Dario Lodeiros
+# Copyright 2020  Jose Luis Algara
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models, fields, api, _
 import re
@@ -11,6 +12,14 @@ class ResCompany(models.Model):
 
     limit_night_sale = fields.Char('Allow night sale until...',
                                    help="HH:mm Format", default="9:00")
+    rm_credit_journal = fields.Many2one('account.journal',
+                                        string='Payment credit card journal',
+                                        help='RoomMatik credit card payment journal.',
+                                        default=0)
+    rm_cash_journal = fields.Many2one('account.journal',
+                                      string='Payment cash journal',
+                                      help='RoomMatik cash payment journal.',
+                                      default=0)
 
     @api.constrains('limit_night_sale',)
     def _check_hours(self):
